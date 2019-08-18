21343ecebc827cf78bb61c281648b9a100c0f239
Guy Segev <gsegev@atlassian.com>
2019-08-18 17:16:52 +1000

    adds silenceRatio connections

--- src/utils/videoController.ts
+++ src/utils/videoController.ts
@@ -10,13 +10,17 @@ class AudioController {
     analyser: any
     speedBuffer: Buffer
     volumeBuffer: Buffer
+    _threshold: number
+    _volume: number
     constructor(video, sampleSize: number){
-        this.analyser = this.initAnalyser(video);
+        this.analyser = this._initAnalyser(video);
         this.speedBuffer = new Buffer(sampleSize);
         this.volumeBuffer = new Buffer(sampleSize); 
+        this._threshold = 0;
+        this._volume = 0;
     }
 
-    initAnalyser(video){
+    _initAnalyser(video){
         const context = new AudioContext();
         const videoMedia = context.createMediaElementSource(video);
 
@@ -29,7 +33,7 @@ class AudioController {
         return analyser;
     }
 
-    getVolume():number{
+    _getVolume():number{
         let soundData = new Uint8Array(this.analyser.fftSize);
         this.analyser.getByteTimeDomainData(soundData);
 
@@ -41,21 +45,36 @@ class AudioController {
         return average/50
     }
 
-    getSilenceThreshold():number{
+    _getSilenceThreshold():number{
         const avgVolume = arrayAvg(this.volumeBuffer.get());
-        console.log('average volume: ', avgVolume)
         // Volumes and Speeds for all below average volume
         const [cleanVolumes, cleanSpeed]:[number[], number[]] = 
             unzip(zip(this.volumeBuffer.get(), this.speedBuffer.get())
             .filter(t => t[0]<avgVolume && t[0]));
-        
+
+        if (cleanVolumes.length === 0) return 0; // if no items in buffer, return 0
         return weightedMedian(cleanVolumes, cleanSpeed)
     }
 
+    // run this on each tick
     update(speed: number){
+        this._volume = this._getVolume();
+
         // update buffers (needs to get speed from the VideoController)
         this.speedBuffer.push(speed);
-        this.volumeBuffer.push(this.getVolume());
+        this.volumeBuffer.push(this._volume);
+
+        this._threshold = this._getSilenceThreshold();
+    }
+
+    isSilent():boolean{
+        return this._volume < this._threshold;
+    }
+
+    getSilenceRatio():number{
+        // how far off the silence ratio we are
+        const silenceDiff = this._threshold - this._volume;
+        return this._threshold === 0 ? 0 : silenceDiff / this._threshold;
     }
 }
 
@@ -97,7 +116,19 @@ export default class VideoController {
             this.audioController.update(this.getSpeed());
             const ac = this.audioController;
 
-            console.log(ac.getVolume(), ac.getSilenceThreshold())
+            const isSilent = ac.isSilent();
+            const silenceRatio = ac.getSilenceRatio();
+
+            if (isSilent){
+                // increment silence len (for padding)
+                // if silence length > time threshold
+                // const dynamicSpeed = settings.regularSpeed + (settings.silenceSpeed - settings.regularSpeed)*silenceRatio; 
+                // this.setSpeed(dynamicSpeed);
+            } else{
+                // silence length = 0
+                // this.setSpeed(regularSpeed)
+            }
+
         } else {
             console.log('no audio controller set');
         }
